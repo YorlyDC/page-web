95a48acbe4571ec7fb03ee7c0701c9a1c38d9f92
Jhanck01-web <63616397+Jhanck01-web@users.noreply.github.com>
2023-06-06 04:29:19 -0400

    script.js

--- script.js
+++ script.js
@@ -1,30 +1,79 @@
-// Selecciona todas las imágenes con el atributo data-src
-const lazyImages = document.querySelectorAll('img[data-src]');
+// Script JavaScript para controlar el slider
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slider-slide");
+const navItems = document.querySelectorAll(".slider-nav-item");
+const overlay = document.querySelectorAll(".slider-overlay");
+let currentIndex = 0;
+let autoSlideInterval;
+let isAutoSlidePaused = false;
+let autoSlideTimeout;
 
-// Crea una instancia del Intersection Observer
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            const img = entry.target;
-            const src = img.getAttribute('data-src');
+// Función para mostrar la imagen actual
+function showSlide(index) {
+  slider.style.transform = `translateX(-${index * 100}%)`;
+  navItems.forEach((item, i) => {
+    item.classList.toggle("active", i === index);
+  });
 
-            // Crea una nueva imagen
-            const newImg = new Image();
-            newImg.src = src;
+  overlay.forEach((item, i) => {
+    item.style.opacity = i === index ? 1 : 0;
+    item.style.pointerEvents = i === index ? "auto" : "none";
+  });
+}
 
-            // Reemplaza la imagen existente con la nueva imagen cargada
-            newImg.onload = () => {
-                img.src = newImg.src;
-                img.removeAttribute('data-src');
-            };
+// Función para cambiar a la siguiente imagen
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+}
 
-            // Deja de observar la imagen una vez que se haya cargado
-            observer.unobserve(img);
-        }
-    });
+// Función para iniciar el desplazamiento automático
+function startAutoSlide() {
+  autoSlide();
+}
+
+function autoSlide() {
+  if (!isAutoSlidePaused) {
+    autoSlideTimeout = setTimeout(() => {
+      nextSlide();
+      autoSlide();
+    }, 8000);
+  }
+}
+
+// Función para detener el desplazamiento automático
+function stopAutoSlide() {
+  clearTimeout(autoSlideTimeout);
+}
+
+// Agregar eventos táctiles para dispositivos móviles
+slider.addEventListener("touchstart", (event) => {
+  touchstartX = event.touches[0].clientX;
+  stopAutoSlide();
 });
 
-// Observa cada imagen
-lazyImages.forEach((img) => {
-    observer.observe(img);
+slider.addEventListener("touchend", (event) => {
+  touchendX = event.changedTouches[0].clientX;
+  if (touchstartX - touchendX > 50) {
+    nextSlide();
+  } else if (touchendX - touchstartX > 50) {
+    currentIndex = currentIndex === 0 ? slides.length - 1 : currentIndex - 1;
+    showSlide(currentIndex);
+  }
+  startAutoSlide();
 });
+
+// Agregar eventos para pausar/reanudar el desplazamiento automático
+slider.addEventListener("mouseenter", () => {
+  isAutoSlidePaused = true;
+  stopAutoSlide();
+});
+
+slider.addEventListener("mouseleave", () => {
+  isAutoSlidePaused = false;
+  autoSlide();
+});
+
+// Mostrar la primera imagen y empezar el desplazamiento automático
+showSlide(currentIndex);
+startAutoSlide();
