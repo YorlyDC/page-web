d1ddf74b9fcaa77873bf577512285d5023d35434
Jhanck01-web <63616397+Jhanck01-web@users.noreply.github.com>
2023-06-08 03:26:01 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,58 @@
 const swiper = new Swiper(".swiper-hero", {
-    // Optional parameters
-    direction: "horizontal",
-    loop: true,
-    effect: "fade",
-    autoplay: {
-        delay: 5000,
-        pauseOnMouseEnter: true,
-        disableOnInteraction: false,
-    },
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  effect: "fade",
+  autoplay: {
+    delay: 5000,
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+  },
 
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-      dynamicBullets: true,
-      dynamicMainBullets: 2,
-    },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    //dynamicBullets: true,
+    //dynamicMainBullets: 2,
+  },
 
-    // Navigation arrows
-    //navigation: {
-    //    nextEl: '.swiper-button-next',
-    //    prevEl: '.swiper-button-prev',
-    //  },
+  // Navigation arrows
+  //navigation: {
+  //    nextEl: '.swiper-button-next',
+  //    prevEl: '.swiper-button-prev',
+  //  },
 
-    // And if we need scrollbar
-    //scrollbar: {
-    //  el: ".swiper-scrollbar",
-    //},
-  });
+  // And if we need scrollbar
+  //scrollbar: {
+  //  el: ".swiper-scrollbar",
+  //},
+});
+
+const swiper2 = new Swiper(".swiper-rectangula", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  effect: "coverflow",
+  grid: {
+    rows: 1,
+    colum: 1,
+  },
+
+  // If we need pagination
+  //pagination: {
+  //  el: ".swiper-pagination",
+  //  dynamicBullets: true,
+  //  dynamicMainBullets: 2,
+  //}
+
+  // Navigation arrows
+  //navigation: {
+  //    nextEl: '.swiper-button-next',
+  //    prevEl: '.swiper-button-prev',
+  //  },
+
+  // And if we need scrollbar
+  //scrollbar: {
+  //  el: ".swiper-scrollbar",
+  //},
+});
